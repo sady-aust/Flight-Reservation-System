de681f9337f6b0175d72796e150e0406a065ffa0
Md. Toufiqul Islam <sadydic@gmail.com>
2018-02-08 18:37:50 +0600

    SQL File Added

--- build/classes/flightreservationsystem.sql
+++ build/classes/flightreservationsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2018 at 03:30 AM
+-- Generation Time: Feb 08, 2018 at 01:37 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -37,6 +37,13 @@ CREATE TABLE `admin` (
   `LastName` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`AdminId`, `AdminUserName`, `AdminPassword`, `Email`, `FirstName`, `LastName`) VALUES
+(1, 'admin', '123', 'admin@gmail.com', 'Toufiqul', 'Islam');
+
 -- --------------------------------------------------------
 
 --
@@ -47,9 +54,47 @@ CREATE TABLE `airlines` (
   `AirlinesId` int(11) NOT NULL,
   `AirlinesName` varchar(300) NOT NULL,
   `AdminId` int(11) NOT NULL,
-  `FlightAirlinesId` int(11) NOT NULL,
-  `CountryName` varchar(300) NOT NULL
+  `CountryName` varchar(300) NOT NULL,
+  `LicenseNo` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `airlines`
+--
+
+INSERT INTO `airlines` (`AirlinesId`, `AirlinesName`, `AdminId`, `CountryName`, `LicenseNo`) VALUES
+(4, 'Biman Bangladesh Airlines', 1, 'Bangladesh', '123-456-789');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airport`
+--
+
+CREATE TABLE `airport` (
+  `AirportId` int(11) NOT NULL,
+  `AirportName` varchar(500) NOT NULL,
+  `CountryName` varchar(500) NOT NULL,
+  `CityName` varchar(300) NOT NULL,
+  `AirportCode` varchar(300) NOT NULL,
+  `AdminId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `airport`
+--
+
+INSERT INTO `airport` (`AirportId`, `AirportName`, `CountryName`, `CityName`, `AirportCode`, `AdminId`) VALUES
+(4, 'Netaji Subhas Chandra Bose International Airport', 'India', 'Kolkata', 'CCU', 1),
+(5, 'Hazrat Shahjalal International Airport', 'Bangladesh', 'Dhaka', 'DAC', 1),
+(6, 'Osmani International Airport', 'Bangladesh', 'Sylhet', 'ZYL', 1),
+(7, 'Shah Amanat International Airport', 'Bangladesh', 'Chittagong', 'CGP', 1),
+(8, 'Paro Airport', 'Bhutan', 'Paro', 'PBH', 1),
+(9, 'Veer Savarkar International Airport', 'India', 'Andaman & Nicobar Islands', 'IXZ', 1),
+(10, 'Veer Savarkar International Airport', 'India', 'Ahmedabad', 'AMD', 1),
+(11, 'Sri Guru Ram Dass Jee International Airport', 'India', 'Amritsar', 'ATQ', 1),
+(12, 'Kempegowda International Airport', 'India', 'Bangalore', 'BLR', 1),
+(13, 'New Chennai International Airport', 'India', 'Chennai', 'NMA', 1);
 
 -- --------------------------------------------------------
 
@@ -67,14 +112,37 @@ CREATE TABLE `class` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `flightairlines`
+-- Table structure for table `feeds`
 --
 
-CREATE TABLE `flightairlines` (
-  `FlightAirlinesId` int(11) NOT NULL,
-  `FlightId` int(11) NOT NULL,
-  `AirlinesId` int(11) NOT NULL
+CREATE TABLE `feeds` (
+  `FeedId` int(11) NOT NULL,
+  `AdminId` int(11) NOT NULL,
+  `Events` varchar(300) DEFAULT NULL,
+  `Date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feeds`
+--
+
+INSERT INTO `feeds` (`FeedId`, `AdminId`, `Events`, `Date`) VALUES
+(1, 1, 'Registration of Biman Bangladesh Airlines', '2018-02-03'),
+(2, 1, 'Registration of Netaji Subhas Chandra Bose International Airport', '2018-02-04'),
+(3, 1, 'Registration of Hazrat Shahjalal International Airport', '2018-02-04'),
+(4, 1, 'Registration of Osmani International Airport', '2018-02-04'),
+(5, 1, 'Registration of Shah Amanat International Airport', '2018-02-04'),
+(6, 1, 'Registration of Paro Airport', '2018-02-04'),
+(7, 1, 'Registration of Veer Savarkar International Airport', '2018-02-04'),
+(8, 1, 'Registration of Veer Savarkar International Airport', '2018-02-04'),
+(9, 1, 'Registration of Sri Guru Ram Dass Jee International Airport', '2018-02-04'),
+(10, 1, 'Registration of Kempegowda International Airport', '2018-02-04'),
+(11, 1, 'Registration of New Chennai International Airport', '2018-02-04'),
+(12, 1, 'Added root id 0', '2018-02-04'),
+(13, 1, 'Added NMA DAC', '2018-02-05'),
+(14, 1, 'Added a new Flight BG-101', '2018-02-08'),
+(15, 1, 'Added a new Flight BG-101', '2018-02-08'),
+(16, 1, 'Added a new Flight BG-102', '2018-02-08');
 
 -- --------------------------------------------------------
 
@@ -108,13 +176,19 @@ CREATE TABLE `flightroot` (
 
 CREATE TABLE `flights` (
   `FlightId` int(11) NOT NULL,
-  `FlightAirlinesId` int(11) NOT NULL,
-  `DepartureTime` time NOT NULL,
-  `DepartureDate` date NOT NULL,
-  `AdminId` int(11) NOT NULL,
-  `FlightRootId` int(11) NOT NULL,
-  `FlightClassId` int(11) NOT NULL
+  `AirlinesId` int(11) NOT NULL,
+  `FLightNumber` varchar(300) NOT NULL,
+  `RootId` int(11) NOT NULL,
+  `AdminId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `flights`
+--
+
+INSERT INTO `flights` (`FlightId`, `AirlinesId`, `FLightNumber`, `RootId`, `AdminId`) VALUES
+(11, 4, 'BG-101', 1, 1),
+(12, 4, 'BG-102', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -139,10 +213,22 @@ CREATE TABLE `payment` (
 
 CREATE TABLE `root` (
   `RootId` int(11) NOT NULL,
-  `DeparturePlace` varchar(300) NOT NULL,
-  `DestinationPlace` varchar(300) NOT NULL,
-  `FlightRootId` int(11) NOT NULL
+  `DepartureCityAirportCode` varchar(300) NOT NULL,
+  `DestinationCityAirportCode` varchar(300) NOT NULL,
+  `AdminId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `root`
+--
+
+INSERT INTO `root` (`RootId`, `DepartureCityAirportCode`, `DestinationCityAirportCode`, `AdminId`) VALUES
+(1, 'DAC', 'CCU', 1),
+(2, 'DAC', 'ZYL', 1),
+(3, 'CCU', 'DAC', 1),
+(4, 'ZYL', 'CCU', 1),
+(5, 'DAC', 'PBH', 1),
+(6, 'NMA', 'DAC', 1);
 
 -- --------------------------------------------------------
 
@@ -153,9 +239,34 @@ CREATE TABLE `root` (
 CREATE TABLE `seat` (
   `SeatId` int(11) NOT NULL,
   `ClassId` int(11) NOT NULL,
-  `FlightId` int(11) NOT NULL,
+  `FlightNumber` varchar(300) NOT NULL,
   `TicketId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seatinfo`
+--
+
+CREATE TABLE `seatinfo` (
+  `SeatInfoId` int(11) NOT NULL,
+  `FirstClass` int(11) DEFAULT '0',
+  `BusinessClass` int(11) DEFAULT '0',
+  `EconomoyClass` int(11) DEFAULT '0',
+  `FLightNumber` varchar(300) NOT NULL,
+  `FirstClassSeatPrice` int(11) DEFAULT NULL,
+  `BusinessClassSeatPrice` int(11) DEFAULT NULL,
+  `EconomyClassSeatPrice` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `seatinfo`
+--
+
+INSERT INTO `seatinfo` (`SeatInfoId`, `FirstClass`, `BusinessClass`, `EconomoyClass`, `FLightNumber`, `FirstClassSeatPrice`, `BusinessClassSeatPrice`, `EconomyClassSeatPrice`) VALUES
+(7, 50, 30, 25, 'BG-101', 4500, 700, 5000),
+(8, 50, 25, 40, 'BG-102', 4500, 7000, 5500);
 
 -- --------------------------------------------------------
 
@@ -170,6 +281,55 @@ CREATE TABLE `ticket` (
   `SeatId` int(11) NOT NULL,
   `BookingDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timeanddate`
+--
+
+CREATE TABLE `timeanddate` (
+  `TimeAndDateId` int(11) NOT NULL,
+  `DepartureDay` varchar(100) NOT NULL,
+  `DepartureTime` varchar(100) NOT NULL,
+  `ArrivalDay` varchar(100) NOT NULL,
+  `ArrivalTime` varchar(100) NOT NULL,
+  `FlightNumber` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `timeanddate`
+--
+
+INSERT INTO `timeanddate` (`TimeAndDateId`, `DepartureDay`, `DepartureTime`, `ArrivalDay`, `ArrivalTime`, `FlightNumber`) VALUES
+(8, 'Monday', '22:00:00', 'Tuesday', '22:30:00', 'BG-101'),
+(9, 'Monday', '22:00:00', 'Tuesday', '22:30:00', 'BG-101'),
+(10, 'Monday', '22:00:00', 'Tuesday', '22:30:00', 'BG-101'),
+(11, 'Wednesday', '17:30:00', 'Wednesday', '17:30:00', 'BG-102'),
+(12, 'Wednesday', '17:30:00', 'Wednesday', '17:30:00', 'BG-102'),
+(13, 'Wednesday', '17:30:00', 'Wednesday', '17:30:00', 'BG-102'),
+(14, 'Wednesday', '17:30:00', 'Wednesday', '17:30:00', 'BG-102');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transitinfo`
+--
+
+CREATE TABLE `transitinfo` (
+  `TransitId` int(11) NOT NULL,
+  `FlightNumber` varchar(300) NOT NULL,
+  `FirstTransit` varchar(100) DEFAULT NULL,
+  `SecondTransit` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transitinfo`
+--
+
+INSERT INTO `transitinfo` (`TransitId`, `FlightNumber`, `FirstTransit`, `SecondTransit`) VALUES
+(8, 'BG-101', 'CGP', NULL),
+(9, 'BG-102', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -205,15 +365,24 @@ INSERT INTO `user` (`User_id`, `user_name`, `Passord`, `FirstName`, `LastName`, 
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`AdminId`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD UNIQUE KEY `AdminUserName` (`AdminUserName`);
 
 --
 -- Indexes for table `airlines`
 --
 ALTER TABLE `airlines`
   ADD PRIMARY KEY (`AirlinesId`),
-  ADD KEY `AdminId` (`AdminId`),
-  ADD KEY `FlightAirlinesId` (`FlightAirlinesId`);
+  ADD UNIQUE KEY `LicenseNo` (`LicenseNo`),
+  ADD KEY `AdminId` (`AdminId`);
+
+--
+-- Indexes for table `airport`
+--
+ALTER TABLE `airport`
+  ADD PRIMARY KEY (`AirportId`),
+  ADD UNIQUE KEY `AirportCode` (`AirportCode`),
+  ADD KEY `AdminId` (`AdminId`);
 
 --
 -- Indexes for table `class`
@@ -223,12 +392,11 @@ ALTER TABLE `class`
   ADD KEY `FlightClassId` (`FlightClassId`);
 
 --
--- Indexes for table `flightairlines`
+-- Indexes for table `feeds`
 --
-ALTER TABLE `flightairlines`
-  ADD PRIMARY KEY (`FlightAirlinesId`),
-  ADD KEY `AirlinesId` (`AirlinesId`),
-  ADD KEY `FlightId` (`FlightId`);
+ALTER TABLE `feeds`
+  ADD PRIMARY KEY (`FeedId`),
+  ADD KEY `AdminId` (`AdminId`);
 
 --
 -- Indexes for table `flightclass`
@@ -251,10 +419,10 @@ ALTER TABLE `flightroot`
 --
 ALTER TABLE `flights`
   ADD PRIMARY KEY (`FlightId`),
-  ADD KEY `FlightAirlinesId` (`FlightAirlinesId`),
-  ADD KEY `AdminId` (`AdminId`),
-  ADD KEY `FlightRootId` (`FlightRootId`),
-  ADD KEY `FlightClassId` (`FlightClassId`);
+  ADD UNIQUE KEY `FLightNumber` (`FLightNumber`),
+  ADD KEY `AirlinesId` (`AirlinesId`),
+  ADD KEY `RootId` (`RootId`),
+  ADD KEY `AdminId` (`AdminId`);
 
 --
 -- Indexes for table `payment`
@@ -268,7 +436,9 @@ ALTER TABLE `payment`
 --
 ALTER TABLE `root`
   ADD PRIMARY KEY (`RootId`),
-  ADD KEY `FlightRootId` (`FlightRootId`);
+  ADD KEY `DepartureCityAirportCode` (`DepartureCityAirportCode`),
+  ADD KEY `DestinationCityAirportCode` (`DestinationCityAirportCode`),
+  ADD KEY `AdminId` (`AdminId`);
 
 --
 -- Indexes for table `seat`
@@ -276,8 +446,15 @@ ALTER TABLE `root`
 ALTER TABLE `seat`
   ADD PRIMARY KEY (`SeatId`),
   ADD KEY `ClassId` (`ClassId`),
-  ADD KEY `FlightId` (`FlightId`),
+  ADD KEY `FlightId` (`FlightNumber`),
   ADD KEY `TicketId` (`TicketId`);
+
+--
+-- Indexes for table `seatinfo`
+--
+ALTER TABLE `seatinfo`
+  ADD PRIMARY KEY (`SeatInfoId`),
+  ADD KEY `FLightNumber` (`FLightNumber`);
 
 --
 -- Indexes for table `ticket`
@@ -286,6 +463,22 @@ ALTER TABLE `ticket`
   ADD PRIMARY KEY (`TicketId`),
   ADD KEY `SeatId` (`SeatId`),
   ADD KEY `PaymentId` (`PaymentId`);
+
+--
+-- Indexes for table `timeanddate`
+--
+ALTER TABLE `timeanddate`
+  ADD PRIMARY KEY (`TimeAndDateId`),
+  ADD KEY `FlightNumber` (`FlightNumber`);
+
+--
+-- Indexes for table `transitinfo`
+--
+ALTER TABLE `transitinfo`
+  ADD PRIMARY KEY (`TransitId`),
+  ADD KEY `FlightNumber` (`FlightNumber`),
+  ADD KEY `FirstTransit` (`FirstTransit`),
+  ADD KEY `SecondTransit` (`SecondTransit`);
 
 --
 -- Indexes for table `user`
@@ -304,13 +497,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `AdminId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AdminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `airlines`
 --
 ALTER TABLE `airlines`
-  MODIFY `AirlinesId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AirlinesId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `airport`
+--
+ALTER TABLE `airport`
+  MODIFY `AirportId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `class`
@@ -319,10 +518,10 @@ ALTER TABLE `class`
   MODIFY `ClassId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `flightairlines`
+-- AUTO_INCREMENT for table `feeds`
 --
-ALTER TABLE `flightairlines`
-  MODIFY `FlightAirlinesId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `feeds`
+  MODIFY `FeedId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `flightclass`
@@ -340,7 +539,7 @@ ALTER TABLE `flightroot`
 -- AUTO_INCREMENT for table `flights`
 --
 ALTER TABLE `flights`
-  MODIFY `FlightId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `FlightId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `payment`
@@ -352,7 +551,7 @@ ALTER TABLE `payment`
 -- AUTO_INCREMENT for table `root`
 --
 ALTER TABLE `root`
-  MODIFY `RootId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `RootId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `seat`
@@ -361,10 +560,28 @@ ALTER TABLE `seat`
   MODIFY `SeatId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `seatinfo`
+--
+ALTER TABLE `seatinfo`
+  MODIFY `SeatInfoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `ticket`
 --
 ALTER TABLE `ticket`
   MODIFY `TicketId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `timeanddate`
+--
+ALTER TABLE `timeanddate`
+  MODIFY `TimeAndDateId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `transitinfo`
+--
+ALTER TABLE `transitinfo`
+  MODIFY `TransitId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -380,8 +597,13 @@ ALTER TABLE `user`
 -- Constraints for table `airlines`
 --
 ALTER TABLE `airlines`
-  ADD CONSTRAINT `airlines_ibfk_1` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`),
-  ADD CONSTRAINT `airlines_ibfk_2` FOREIGN KEY (`FlightAirlinesId`) REFERENCES `flightairlines` (`FlightAirlinesId`);
+  ADD CONSTRAINT `airlines_ibfk_1` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`);
+
+--
+-- Constraints for table `airport`
+--
+ALTER TABLE `airport`
+  ADD CONSTRAINT `airport_ibfk_1` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`);
 
 --
 -- Constraints for table `class`
@@ -390,36 +612,18 @@ ALTER TABLE `class`
   ADD CONSTRAINT `class_ibfk_1` FOREIGN KEY (`FlightClassId`) REFERENCES `flightclass` (`FlightClassId`);
 
 --
--- Constraints for table `flightairlines`
+-- Constraints for table `feeds`
 --
-ALTER TABLE `flightairlines`
-  ADD CONSTRAINT `flightairlines_ibfk_1` FOREIGN KEY (`AirlinesId`) REFERENCES `airlines` (`AirlinesId`),
-  ADD CONSTRAINT `flightairlines_ibfk_2` FOREIGN KEY (`FlightId`) REFERENCES `flights` (`FlightId`);
-
---
--- Constraints for table `flightclass`
---
-ALTER TABLE `flightclass`
-  ADD CONSTRAINT `flightclass_ibfk_1` FOREIGN KEY (`FlightId`) REFERENCES `flights` (`FlightId`),
-  ADD CONSTRAINT `flightclass_ibfk_2` FOREIGN KEY (`ClassId`) REFERENCES `class` (`ClassId`);
-
---
--- Constraints for table `flightroot`
---
-ALTER TABLE `flightroot`
-  ADD CONSTRAINT `flightroot_ibfk_1` FOREIGN KEY (`RootId`) REFERENCES `root` (`RootId`),
-  ADD CONSTRAINT `flightroot_ibfk_2` FOREIGN KEY (`FLightId`) REFERENCES `flights` (`FlightId`);
+ALTER TABLE `feeds`
+  ADD CONSTRAINT `feeds_ibfk_1` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`);
 
 --
 -- Constraints for table `flights`
 --
 ALTER TABLE `flights`
-  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`FlightAirlinesId`) REFERENCES `flightairlines` (`FlightAirlinesId`),
-  ADD CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`),
-  ADD CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`FlightRootId`) REFERENCES `flightroot` (`FlightRootId`),
-  ADD CONSTRAINT `flights_ibfk_4` FOREIGN KEY (`FlightClassId`) REFERENCES `flightclass` (`FlightClassId`),
-  ADD CONSTRAINT `flights_ibfk_5` FOREIGN KEY (`FlightClassId`) REFERENCES `flightclass` (`FlightClassId`),
-  ADD CONSTRAINT `flights_ibfk_6` FOREIGN KEY (`FlightClassId`) REFERENCES `flightclass` (`FlightClassId`);
+  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`AirlinesId`) REFERENCES `airlines` (`AirlinesId`),
+  ADD CONSTRAINT `flights_ibfk_2` FOREIGN KEY (`RootId`) REFERENCES `root` (`RootId`),
+  ADD CONSTRAINT `flights_ibfk_3` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`);
 
 --
 -- Constraints for table `payment`
@@ -431,15 +635,22 @@ ALTER TABLE `payment`
 -- Constraints for table `root`
 --
 ALTER TABLE `root`
-  ADD CONSTRAINT `root_ibfk_1` FOREIGN KEY (`FlightRootId`) REFERENCES `flightroot` (`FlightRootId`);
+  ADD CONSTRAINT `root_ibfk_1` FOREIGN KEY (`DepartureCityAirportCode`) REFERENCES `airport` (`AirportCode`),
+  ADD CONSTRAINT `root_ibfk_2` FOREIGN KEY (`DestinationCityAirportCode`) REFERENCES `airport` (`AirportCode`),
+  ADD CONSTRAINT `root_ibfk_3` FOREIGN KEY (`AdminId`) REFERENCES `admin` (`AdminId`);
 
 --
 -- Constraints for table `seat`
 --
 ALTER TABLE `seat`
   ADD CONSTRAINT `seat_ibfk_1` FOREIGN KEY (`ClassId`) REFERENCES `class` (`ClassId`),
-  ADD CONSTRAINT `seat_ibfk_2` FOREIGN KEY (`FlightId`) REFERENCES `flights` (`FlightId`),
   ADD CONSTRAINT `seat_ibfk_3` FOREIGN KEY (`TicketId`) REFERENCES `ticket` (`TicketId`);
+
+--
+-- Constraints for table `seatinfo`
+--
+ALTER TABLE `seatinfo`
+  ADD CONSTRAINT `seatinfo_ibfk_1` FOREIGN KEY (`FLightNumber`) REFERENCES `flights` (`FLightNumber`);
 
 --
 -- Constraints for table `ticket`
@@ -447,6 +658,20 @@ ALTER TABLE `seat`
 ALTER TABLE `ticket`
   ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`SeatId`) REFERENCES `seat` (`SeatId`),
   ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`PaymentId`) REFERENCES `payment` (`PaymentId`);
+
+--
+-- Constraints for table `timeanddate`
+--
+ALTER TABLE `timeanddate`
+  ADD CONSTRAINT `timeanddate_ibfk_1` FOREIGN KEY (`FlightNumber`) REFERENCES `flights` (`FLightNumber`);
+
+--
+-- Constraints for table `transitinfo`
+--
+ALTER TABLE `transitinfo`
+  ADD CONSTRAINT `transitinfo_ibfk_1` FOREIGN KEY (`FlightNumber`) REFERENCES `flights` (`FLightNumber`),
+  ADD CONSTRAINT `transitinfo_ibfk_2` FOREIGN KEY (`FirstTransit`) REFERENCES `airport` (`AirportCode`),
+  ADD CONSTRAINT `transitinfo_ibfk_3` FOREIGN KEY (`SecondTransit`) REFERENCES `airport` (`AirportCode`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
